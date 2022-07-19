1483c812eb00ffe8ae08e04e02df9329331b377d
nithinneel <105333944+nithinneel@users.noreply.github.com>
2022-07-19 12:52:01 +0530

    Update ExampleScript3Test.java

--- com.MavenProjectWithTestNG/src/test/java/com/Example/Test/ExampleScript3Test.java
+++ com.MavenProjectWithTestNG/src/test/java/com/Example/Test/ExampleScript3Test.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 public class ExampleScript3Test {
 	@Test
 	public void Ex3Test1() {
-		System.out.println("this is Ex-3Test1");
+		System.out.println("Hi...this is Ex-3Test1");
+		
+		
 	}
 }
